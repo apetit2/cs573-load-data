042e3af63582c849073d5fb6a29c8f895de6bd4f
Andrew Petit <andrew.petit2@gmail.com>
2021-09-03 17:30:33 -0400

    setting up travis

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ['airbnb-typescript-prettier'],
+    rules: {
+      'react/prop-types': 0,
+      'react/destructuring-assignment': 0,
+      'react/static-property-placement': 0,
+      'jsx-a11y/alt-text': 0,
+      'react/jsx-props-no-spreading': 0,
+      '@typescript-eslint/explicit-module-boundary-types': 0,
+    },
+  };
